9c7fc3cacf9234b11ee7b7aca64e1619c348ea27
mlzepplin <rishabh.gupta.1606@gmail.com>
2015-10-01 03:34:27 +0530

    Initial commit UI

---
+++ app/src/main/java/com/arps/imean/HomeScreen.java
@@ -0,0 +1,104 @@
+package com.arps.imean;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class HomeScreen extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+
+        //THE SEARCH EDIT TEXT
+        final String nullcheck = null;// A NULL STRING
+        final EditText searchEditText = (EditText)findViewById(R.id.searchEditText);
+
+
+
+        //SEARCH BUTTON
+        Button searchButton = (Button)findViewById(R.id.searchButton);
+        //EVENT HANDLING
+        searchButton.setOnClickListener(
+
+                //Starting up an interface for an event
+                new Button.OnClickListener() {
+
+                    public void onClick(View v) {
+
+                        if (nullcheck.equals(searchEditText.getText().toString())) {
+
+                            // TOAST
+                            Toast.makeText(HomeScreen.this, "enter a word to search for", Toast.LENGTH_LONG).show();
+
+                        } else {
+
+                            //MEANING ACTIVITY LAUNCHED
+                            Intent launchMeaningIntent = new Intent(HomeScreen.this, MeaningDisplay.class);
+                            startActivity(launchMeaningIntent);
+                        }
+                    }
+
+                }
+        );
+
+        String[] labels = {"positive words","negative words","harsh words","physical description"};
+
+        ListAdapter labelsAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_activated_1,labels);
+        ListView labelsListView = (ListView)findViewById(R.id.labelsListView);
+        labelsListView.setAdapter(labelsAdapter);
+
+        labelsListView.setOnItemClickListener(
+
+                new AdapterView.OnItemClickListener() {
+
+                    public void onItemClick(AdapterView<?> parent,View view, int position, long id) {
+
+                        String label = String.valueOf(parent.getItemAtPosition(position));
+                        Intent launchWordListIntent = new Intent(HomeScreen.this, LabelWordList.class);
+                        startActivity(launchWordListIntent);
+
+                    }
+
+                }
+
+        );
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home_screen, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/arps/imean/LabelWordList.java
@@ -0,0 +1,67 @@
+package com.arps.imean;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+
+public class LabelWordList extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_label_word_list);
+
+
+        String[] words = {"words1","word2","word3","word4"};
+        ListAdapter labelsAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_activated_1,words);
+        ListView labelsListView = (ListView)findViewById(R.id.labelsListView);
+        labelsListView.setAdapter(labelsAdapter);
+
+        labelsListView.setOnItemClickListener(
+
+                new AdapterView.OnItemClickListener() {
+
+                    public void onItemClick(AdapterView<?> parent,View view, int position, long id) {
+
+                        String word = String.valueOf(parent.getItemAtPosition(position));
+                        Intent changeScreenIntent3 = new Intent(LabelWordList.this, OneWordMeaning.class);
+                        startActivity(changeScreenIntent3);
+
+                    }
+
+                }
+
+        );
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_label_word_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
